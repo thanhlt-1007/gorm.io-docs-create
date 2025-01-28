5c5b9fd8ee28b3ea7d1e0c8871b78346f6f663ed
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2025-01-28 20:28:01 +0700

    Create users

--- main.go
+++ main.go
@@ -50,7 +50,33 @@ func CreateUser(db *gorm.DB) {
     fmt.Printf("Birthday: %v\n", user.Birthday)
 }
 
+func CreateUsers(db *gorm.DB) {
+    users := []User {
+        {
+            Name: "Jinzhu 1",
+        },
+        {
+            Name: "Jinzhu 2",
+        },
+    }
+    result := db.Create(&users)
+
+    fmt.Printf("result.Error: %v\n", result.Error)
+    fmt.Printf("result.RowsAffected: %d\n", result.RowsAffected)
+
+    for _, user := range users {
+        fmt.Printf("ID: %d\n", user.ID)
+        fmt.Printf("CreatedAt: %v\n", user.CreatedAt)
+        fmt.Printf("UpdatedAt: %v\n", user.UpdatedAt)
+        fmt.Printf("DeletedAt: %v\n", user.DeletedAt)
+        fmt.Printf("Name: %s\n", user.Name)
+        fmt.Printf("Age: %d\n", user.Age)
+        fmt.Printf("Birthday: %v\n", user.Birthday)
+    }
+}
+
 func main() {
     db := InitDb()
     CreateUser(db)
+    CreateUsers(db)
 }
